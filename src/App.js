b59574031a0707dda24762e95a7698342d68afd2
CeeJay1506 <judithnweke82@gmail.com>
2024-05-12 18:21:03 +0100

    Profolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ReposList from "./components/ReposList";
+import RepoDetails from "./components/RepoDetails";
+import ErrorBoundary from "./components/ErrorBoundary";
+import NotFoundPage from "./components/NotFoundPage";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ErrorBoundary>
+        <Routes>
+          <Route exact path="/" element={<ReposList />} />
+          <Route path="/repo/:repoId" element={<RepoDetails />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </ErrorBoundary>
+    </Router>
   );
-}
+};
 
 export default App;
